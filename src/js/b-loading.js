a786d64097eac907bcbbf6872c6b1d8cb1b28d1c
Elias Biondo <contato@eliasbiondo.com>
2022-09-24 19:10:24 -0300

    :wrench: fix 'b-loading' page

---
+++ src/js/b-loading.js
@@ -0,0 +1,62 @@
+// Current page number element
+currentPageNumberElement = document.querySelector("#current-page-number");
+
+// Current page headline element
+currentPageHeadlineElement = document.querySelector(".headline"); 
+
+// Step counter.
+i = 0;
+
+stages = [
+	{
+		pageNumber: 1,
+		headline: "Conecte suas contas bancárias em um só lugar."
+	},
+	{
+		pageNumber: 2,
+		headline: "Compartilhe dados de dispositivos inteligentes."
+	},
+	{
+		pageNumber: 3,
+		headline: "Receba produtos financeiros personalizados para você."
+	}
+]
+
+
+// Instancing the sleep function.
+async function sleep(ms) {
+
+    return new Promise(resolve => setTimeout(resolve, ms));
+
+}
+
+
+async function exec() {
+
+	// Setting the first stage.
+	currentPageNumberElement.innerText = stages[i].pageNumber;
+	currentPageHeadlineElement.innerText = stages[i].headline;
+	i++;
+
+	// Sleeping for 2 seconds.
+	await sleep(2000)
+
+	// Setting the second stage.
+	currentPageNumberElement.innerText = stages[i].pageNumber;
+	currentPageHeadlineElement.innerText = stages[i].headline;
+	i++;
+
+	// Sleeping for 2 seconds.
+	await sleep(2000)
+
+	// Setting the second stage.
+	currentPageNumberElement.innerText = stages[i].pageNumber;
+	currentPageHeadlineElement.innerText = stages[i].headline;
+
+	// Sleeping for 2 seconds.
+	await sleep(2000)
+
+}
+
+exec();
+
