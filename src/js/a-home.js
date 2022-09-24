c8b4d66722619770c3c30368bbc5ec7965b6123a
Elias Biondo <contato@eliasbiondo.com>
2022-09-24 17:12:46 -0300

    :sparkles: add 'a-home' page.

---
+++ src/js/a-home.js
@@ -0,0 +1,7 @@
+const getStartedBtn = document.querySelector("#get-started-btn");
+
+getStartedBtn.addEventListener("click", event => {
+
+	window.location.href = "http://localhost:5500/html/b-loading.html";
+	
+})
