d0ba3126cb64cd8c4cb50d74cff9d2b9d99dbc0e
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-09-25 00:20:08 -0300

    Update page i

---
+++ src/js/i-buy-payment-account.js
@@ -0,0 +1,12 @@
+const radioBtg = document.querySelector("#radio-btg")
+const radioPan = document.querySelector("#radio-pan")
+
+radioBtg.checked == false;
+radioBtg.checked == false;
+
+if (radioBtg.checked = true){
+    radioPan.checked == false;
+}
+else if(radioPan.checked = true){
+    radioBtg.checked == false;
+}
